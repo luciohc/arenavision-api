d4f7b21334dd350b8e1b22df4e931f7b8f13f008
luciohc <lucio@gmail.com>
2025-07-14 17:37:20 -0300

    🔐 Adiciona middleware de autenticação por API key

--- backend/middleware/apiKey.js
+++ backend/middleware/apiKey.js
@@ -1,7 +1,19 @@
 require('dotenv').config();
 
+// Middleware para verificar chave da API
+app.use((req, res, next) => {
+  const apiKey = req.headers['x-rapidapi-key'];
 
-module.exports = function (req, res, next) {
+  if (!apiKey || apiKey !== '30d754441dmsh2f51d5ab15cafe0p10dd53jsn3e54a7e29d19') {
+    return res.status(401).json({ error: 'Chave de API inválida ou ausente.' });
+  }
+
+  next();
+});
+
+
+
+/*module.exports = function (req, res, next) {
   const userKey = req.headers['x-api-key'];
   const serverKey = process.env.API_KEY;
     
@@ -10,4 +22,4 @@ module.exports = function (req, res, next) {
   }
 
   next();
-};
+};*/
